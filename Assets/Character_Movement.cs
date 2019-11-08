35bf2defd09bb83d360a03aed425a7ba0a0f3e5f
omkrth <omkrth2@naver.com>
2019-11-08 15:01:17 +0900

    Gookbap

---
+++ Assets/Character_Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Character_Movement : MonoBehaviour
+{
+    public float speed = 10f;
+    public Vector2 v = Vector2.zero;
+    Rigidbody2D myRigidbody;
+    
+
+
+    void Start() 
+    {
+        myRigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+
+    private void FixedUpdate() //방향키 조절
+    {
+        v = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            v += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            v += Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            v += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            v += Vector2.down;
+        }
+        v.Normalize();
+
+        
+        myRigidbody.velocity = v * speed;
+    }
+}
---
+++ Assets/Transparency.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transparency : MonoBehaviour
+{
+    SpriteRenderer render;
+    void Start()
+    {
+        render = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) //A키를 눌렀을 때 실행됨
+        {
+            render.color = new Color(1, 1, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S)) //A키를 눌렀을 때 실행됨
+        {
+            render.color = new Color(1, 1, 1, 1);
+        }
+    }
+}
