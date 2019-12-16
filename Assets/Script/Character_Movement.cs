c0d96a9c25d5318d98c0ad9903f7f62ab62d7d60
seolip <khw0410s@gmail.com>
2019-12-16 22:04:05 +0900

    study1

---
+++ Assets/Script/Character_Movement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Character_Movement : MonoBehaviour
+{
+    public float speed = 10f;
+    public float speedInWater = 5f;
+    public static bool onWater = false;
+    public Vector2 v = Vector2.zero;
+    Rigidbody2D myRigidbody;
+    
+
+
+    void Start() 
+    {
+        myRigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+
+    private void FixedUpdate() //방향키 조절
+    {
+        v = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            v += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            v += Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            v += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            v += Vector2.down;
+        }
+        v.Normalize();
+
+        if(onWater)
+            myRigidbody.velocity = v * speedInWater;
+        else
+            myRigidbody.velocity = v * speed;
+
+    }
+
+    private void Update()
+    {
+        Debug.Log(gameObject.layer);
+    }
+}
---
+++ Assets/Script/Moving_Skills.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+
+
+public class Moving_Skills : MonoBehaviour
+{
+    Rigidbody2D myRigidbody;
+    public float dashPower = 100f;
+    private float coolTime = 5;
+
+    void Start()
+    {
+        myRigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {     
+        if (Input.GetKeyDown(KeyCode.Z) && coolTime > 5) //Z키를 눌렀을 때 실행됨
+        {
+            Debug.Log(myRigidbody.velocity.normalized); //Z키가 입력되면 Console 창에 속도가 표시됨
+            myRigidbody.AddForce(myRigidbody.velocity.normalized * dashPower);
+            coolTime = 0;
+        }
+        coolTime += Time.fixedDeltaTime;
+    }
+}
